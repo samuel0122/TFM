5e5673d7e3bbd86bfbfae63cecb419b91961b66f
samuel0122 <samuoliva9@gmail.com>
2024-07-13 16:08:28 +0200

    Created abstract view and viewModel for camera.

---
+++ app/src/main/java/com/mastermovilesua/persistencia/tfm_detectorpentagramas/ui/common/camera/CameraViewModel.kt
@@ -0,0 +1,226 @@
+package com.mastermovilesua.persistencia.tfm_detectorpentagramas.ui.common.camera
+
+import android.content.Context
+import android.net.Uri
+import androidx.annotation.NonNull
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.FocusMeteringAction
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.MeteringPoint
+import androidx.camera.core.Preview
+import androidx.camera.core.resolutionselector.AspectRatioStrategy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mastermovilesua.persistencia.tfm_detectorpentagramas.core.eCameraFacing
+import com.mastermovilesua.persistencia.tfm_detectorpentagramas.core.eCameraState
+import com.mastermovilesua.persistencia.tfm_detectorpentagramas.core.utils.SaveToMediaStore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+open class CameraViewModel @Inject constructor() : ViewModel() {
+    private val _flashOn = MutableLiveData<Boolean>()
+    private val _isCapturingImage = MutableLiveData<Boolean>()
+    private val _cameraFacing = MutableLiveData<eCameraFacing>()
+    private val _pictureUri = MutableLiveData<Uri>()
+    private val _cameraState = MutableLiveData<eCameraState>()
+
+    val flashOn: LiveData<Boolean> get() = _flashOn
+    val isCapturingImage: LiveData<Boolean> get() = _isCapturingImage
+    val cameraFacing: LiveData<eCameraFacing> get() = _cameraFacing
+    val pictureUri: LiveData<Uri> get() = _pictureUri
+    val cameraState: LiveData<eCameraState> get() = _cameraState
+
+    private var _cameraProvider: ProcessCameraProvider? = null
+    private var _camera: Camera? = null
+
+    val camera: Camera? get() = _camera
+    val cameraProvider: ProcessCameraProvider? get() = _cameraProvider
+
+    lateinit var preview: Preview
+    lateinit var imageCapture: ImageCapture
+    lateinit var cameraSelector: CameraSelector
+
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
+    private var aspectRatio = AspectRatio.RATIO_16_9
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    fun onCreate() {
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        pictureUri.value?.let { pictureUriValue -> _pictureUri.postValue(pictureUriValue) }
+
+        buildCameraSelector()
+    }
+
+    fun onStartCamera(rotation: Int) {
+        viewModelScope.launch {
+            buildImageCapture(rotation)
+
+            buildPreview(rotation)
+
+            _cameraState.postValue(cameraState.value ?: eCameraState.Live)
+            _flashOn.postValue(flashOn.value ?: false)
+            _isCapturingImage.postValue(isCapturingImage.value ?: false)
+            _cameraFacing.postValue(cameraFacing.value ?: eCameraFacing.Back)
+        }
+    }
+
+    fun onDestroy() {
+        cameraExecutor.shutdown()
+        cameraProvider?.unbindAll()
+    }
+
+    private fun buildPreview(rotation: Int) {
+        val resolutionSelector = ResolutionSelector.Builder()
+            .setAspectRatioStrategy(
+                AspectRatioStrategy(
+                    aspectRatio,
+                    AspectRatioStrategy.FALLBACK_RULE_AUTO
+                )
+            )
+            .build()
+
+        preview = Preview.Builder()
+            .setResolutionSelector(resolutionSelector)
+            .setTargetRotation(rotation)
+            .build()
+    }
+
+    private fun buildImageCapture(rotation: Int) {
+        val resolutionSelector = ResolutionSelector.Builder()
+            .setAspectRatioStrategy(
+                AspectRatioStrategy(
+                    aspectRatio,
+                    AspectRatioStrategy.FALLBACK_RULE_AUTO
+                )
+            )
+            .build()
+
+        imageCapture = ImageCapture.Builder()
+            .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            .setResolutionSelector(resolutionSelector)
+            .setTargetRotation(rotation)
+            .build()
+    }
+
+    private fun buildCameraSelector() {
+        cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(lensFacing)
+            .build()
+    }
+
+    fun discardCapturedPage() {
+        _cameraState.postValue(eCameraState.Live)
+    }
+
+    fun setCameraProvider(cameraProvider: ProcessCameraProvider) {
+        _cameraProvider = cameraProvider
+    }
+
+    fun setCamera(camera: Camera) {
+        _camera = camera
+    }
+
+    fun flipCamera() {
+        cameraFacing.value?.let { cameraFacing ->
+            viewModelScope.launch {
+                when (cameraFacing) {
+                    eCameraFacing.Front -> {
+                        lensFacing = CameraSelector.LENS_FACING_BACK
+                        _cameraFacing.postValue(eCameraFacing.Back)
+                    }
+
+                    eCameraFacing.Back -> {
+                        lensFacing = CameraSelector.LENS_FACING_FRONT
+                        _cameraFacing.postValue(eCameraFacing.Front)
+                    }
+                }
+
+                buildCameraSelector()
+            }
+        }
+    }
+
+    fun toggleFlash() {
+        viewModelScope.launch {
+            camera?.let { camera ->
+                if (camera.cameraInfo.hasFlashUnit()) {
+                    val turnFlashOn = _flashOn.value == false
+
+                    camera.cameraControl.enableTorch(turnFlashOn)
+                    _flashOn.postValue(turnFlashOn)
+                }
+            }
+        }
+    }
+
+    fun updateExposure(value: Float) {
+        viewModelScope.launch {
+            camera?.let { camera ->
+                val range = camera.cameraInfo.exposureState.exposureCompensationRange
+                val exposure = ((value * 2f) - 1f) * range.upper
+                camera.cameraControl.setExposureCompensationIndex(exposure.toInt())
+            }
+        }
+    }
+
+    fun updateZoom(delta: Float) {
+        viewModelScope.launch {
+            camera?.let { camera ->
+                val currentZoomRatio = camera.cameraInfo.zoomState.value?.zoomRatio ?: 1f
+                camera.cameraControl.setZoomRatio(currentZoomRatio * delta)
+            }
+        }
+    }
+
+    fun focus(point: MeteringPoint) {
+        viewModelScope.launch {
+            val action = FocusMeteringAction.Builder(
+                point,
+                FocusMeteringAction.FLAG_AF or FocusMeteringAction.FLAG_AE
+            )
+                .setAutoCancelDuration(5, TimeUnit.SECONDS)
+                .build()
+            camera?.cameraControl?.startFocusAndMetering(action)
+        }
+    }
+
+    fun takePhoto(@NonNull context: Context) {
+        viewModelScope.launch {
+            _isCapturingImage.postValue(true)
+
+            val file = File(context.filesDir, SaveToMediaStore.getImageFileName())
+
+            val outputOptions = ImageCapture.OutputFileOptions.Builder(file).build()
+
+            imageCapture.takePicture(
+                outputOptions,
+                cameraExecutor,
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                        _pictureUri.postValue(Uri.fromFile(file))
+                        _cameraState.postValue(eCameraState.ImageCaptured)
+                        _isCapturingImage.postValue(false)
+                    }
+
+                    override fun onError(exception: ImageCaptureException) {
+                        _isCapturingImage.postValue(false)
+                    }
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/mastermovilesua/persistencia/tfm_detectorpentagramas/ui/view/AddPageCameraFragment.kt
+++ app/src/main/java/com/mastermovilesua/persistencia/tfm_detectorpentagramas/ui/view/AddPageCameraFragment.kt
@@ -17,203 +17,36 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.mastermovilesua.persistencia.tfm_detectorpentagramas.R
 import com.mastermovilesua.persistencia.tfm_detectorpentagramas.core.eCameraState
 import com.mastermovilesua.persistencia.tfm_detectorpentagramas.core.utils.Permissions
 import com.mastermovilesua.persistencia.tfm_detectorpentagramas.databinding.FragmentCameraBinding
+import com.mastermovilesua.persistencia.tfm_detectorpentagramas.ui.common.camera.CameraFragment
 import com.mastermovilesua.persistencia.tfm_detectorpentagramas.ui.viewModel.AddPageCameraViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class AddPageCameraFragment : Fragment() {
-    private val viewModel: AddPageCameraViewModel by viewModels()
-    private val args: AddPageCameraFragmentArgs by navArgs()
+class AddPageCameraFragment : CameraFragment() {
+    override val viewModel: AddPageCameraViewModel by viewModels()
 
-    private lateinit var binding: FragmentCameraBinding
+    private val args: AddPageCameraFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        Log.d(TAG, "On CREATE VIEW")
-
-        binding = FragmentCameraBinding.inflate(inflater)
-
-        binding.btnCloseCamera.setOnClickListener { findNavController().navigateUp() }
-        binding.btnFlipCamera.setOnClickListener { viewModel.flipCamera() }
-        binding.btnToggleFlash.setOnClickListener { viewModel.toggleFlash() }
-        binding.btnShotPhoto.setOnClickListener { viewModel.takePhoto(requireContext()) }
+       val cameraView = super.onCreateView(inflater, container, savedInstanceState)
 
         binding.btnConfirmPhoto.setOnClickListener {
             viewModel.insertCapturedPage()
             findNavController().navigateUp()
         }
 
-        binding.btnDiscardPhoto.setOnClickListener { viewModel.discardCapturedPage() }
-
-        binding.clLive.visibility = View.GONE
-        binding.clPreview.visibility = View.GONE
-
-        return binding.root
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        Log.d(TAG, "On CREATE")
-
         viewModel.onCreate(args.bookId)
 
-        if (Permissions.hasCameraPermission(requireContext())) {
-            startCamera()
-        } else {
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-                if (isGranted) {
-                    startCamera()
-                } else {
-                    // Manejar el caso en el que no se otorgó el permiso
-                    Toast.makeText(
-                        requireContext(),
-                        "Permission denied. Camera cannot be accessed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    findNavController().navigateUp()
-                }
-            }.launch(Permissions.useCameraPerm)
-        }
-
-        viewModel.cameraFacing.observe(this) { _ ->
-            bindCameraUserCases()
-        }
-
-        viewModel.flashOn.observe(this) { isFlashOn ->
-            if (isFlashOn) binding.btnToggleFlash.setImageResource(R.drawable.ic_flash_on)
-            else binding.btnToggleFlash.setImageResource(R.drawable.ic_flash_off)
-        }
-
-        viewModel.pictureUri.observe(this) { savedUri ->
-            binding.ivPage.setImageURI(savedUri)
-        }
-
-        viewModel.cameraState.observe(this) { cameraState ->
-            when (cameraState) {
-                eCameraState.Live -> {
-                    binding.clLive.visibility = View.VISIBLE
-                    binding.clPreview.visibility = View.GONE
-                }
-
-                eCameraState.ImageCaptured -> {
-                    binding.clLive.visibility = View.GONE
-                    binding.clPreview.visibility = View.VISIBLE
-                }
-            }
-        }
-
-        viewModel.isCapturingImage.observe(this) { isCapturingImage ->
-            if (isCapturingImage) binding.btnShotPhoto.imageTintList = ContextCompat.getColorStateList(requireContext(), R.color.dark_gray)
-            else binding.btnShotPhoto.imageTintList = ContextCompat.getColorStateList(requireContext(), R.color.white)
-        }
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-
-        cameraProviderFuture.addListener({
-            viewModel.setCameraProvider(cameraProviderFuture.get())
-
-            viewModel.onStartCamera(binding.pvCamera.display.rotation)
-
-            viewModel.preview.apply {
-                setSurfaceProvider(binding.pvCamera.surfaceProvider)
-            }
-        }, ContextCompat.getMainExecutor(requireContext()))
-    }
-
-    private fun bindCameraUserCases() {
-        try {
-            viewModel.cameraProvider?.let { cameraProvider ->
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                val camera = cameraProvider.bindToLifecycle(
-                    this,
-                    viewModel.cameraSelector,
-                    viewModel.preview,
-                    viewModel.imageCapture
-                )
-
-                viewModel.setCamera(camera)
-
-                setUpZoomTapToFocusAndExposure()
-            }
-
-        } catch (exc: Exception) {
-            Log.e(TAG, "Use case binding failed", exc)
-        }
-    }
-
-    private fun setUpZoomTapToFocusAndExposure() {
-        // Configuración del control de exposición
-        binding.sbExposure.setProgress(50, true)
-        viewModel.updateExposure(binding.sbExposure.progress.toFloat() / binding.sbExposure.max.toFloat())
-        binding.sbExposure.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                seekBar?.let {
-                    viewModel.updateExposure(progress.toFloat() / seekBar.max.toFloat())
-                }
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        })
-
-        // Configuración del detector de gestos para el zoom
-        val scaleGestureDetector = ScaleGestureDetector(
-            requireContext(),
-            object : ScaleGestureDetector.SimpleOnScaleGestureListener() {
-                override fun onScale(detector: ScaleGestureDetector): Boolean {
-                    viewModel.updateZoom(detector.scaleFactor)
-                    return true
-                }
-            })
-
-        // Configuración del detector de gestos para el enfoque
-        val gestureDetector =
-            GestureDetector(requireContext(), object : GestureDetector.SimpleOnGestureListener() {
-                override fun onSingleTapUp(e: MotionEvent): Boolean {
-                    // Mostrar círculo de enfoque
-                    binding.focusCircleView.setRectPoint(e.x, e.y)
-                    binding.focusCircleView.invalidate()
-
-                    // Focus
-                    val factory = binding.pvCamera.meteringPointFactory
-                    val point = factory.createPoint(e.x, e.y)
-                    viewModel.focus(point)
-
-                    return true
-                }
-            })
-
-        // Configuración del enfoque táctil y el zoom
-        binding.pvCamera.setOnTouchListener { view, event ->
-            scaleGestureDetector.onTouchEvent(event)
-            if (!scaleGestureDetector.isInProgress) {
-                gestureDetector.onTouchEvent(event)
-            }
-            view.performClick()
-            true
-        }
-    }
-
-    override fun onDestroy() {
-        Log.d(TAG, "On DESTROY")
-        viewModel.onDestroy()
-        super.onDestroy()
-    }
-
-    companion object {
-        private const val TAG = "AddPageCameraFragment"
+        return cameraView
     }
 }
